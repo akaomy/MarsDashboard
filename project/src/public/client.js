579f1b8eb64e9dbfec177294144a6448a4c1dbbb
akozlova <annakozlova92@gmail.com>
2021-07-21 14:52:13 -0700

    fix for displaying image

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -2,6 +2,7 @@ let store = {
     user: { name: "Student" },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    roversData: ''
 }
 
 // add our markup to the page
@@ -53,27 +54,21 @@ window.addEventListener('load', () => {
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
 const Greeting = (name) => {
     if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
+        return `<h1>Welcome, ${name}!</h1>`
     }
 
-    return `
-        <h1>Hello!</h1>
-    `
+    return `<h1>Hello!</h1>`
 }
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
 
     // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+    const today = new Date();
+    const photodate = new Date(apod.date);
 
-    console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
+        getImageOfTheDay(store);
     }
 
     // check if the photo of the day is actually type video!
@@ -91,15 +86,31 @@ const ImageOfTheDay = (apod) => {
     }
 }
 
+const outputsAllRowersInfo = (rovers) => {
+    getRoversData(store);
+    console.log(rovers)
+}
+
 // ------------------------------------------------------  API CALLS
 
 // Example API call
 const getImageOfTheDay = (state) => {
     let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
+    fetch(`http://localhost:3010/apod`)
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
+
+    return data
+}
+
+// get all rowers info
+const getRoversData = (state) => {
+    let { rovers } = state
+
+    fetch(`http://localhost:3010/rovers`)
+        .then(res => res.json())
+        .then(rovers => updateStore(store, { rovers }))
 
     return data
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
+require('dotenv').config({ path: '../../.env'})
+console.log(process.env);
 const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = 3010
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -21,7 +22,17 @@ app.get('/apod', async (req, res) => {
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
-        console.log('error:', err);
+        console.log('error:', err)
+    }
+})
+app.get('/rovers', async (req, res) => {
+    try {
+        let rovers = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ rovers })
+        console.log('res rovers', rovers)
+    } catch (err) {
+        console.log('error:', err)
     }
 })
 
